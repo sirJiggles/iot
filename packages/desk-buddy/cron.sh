3ea412cb4ff1aae69888000e1680d3474231210f
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2021-03-19 09:07:32 +0100

    update cron to pull and build on reboot

--- packages/desk-buddy/cron.sh
+++ packages/desk-buddy/cron.sh
@@ -1,2 +1,3 @@
 sleep 10
+cd /home/pi/Desktop/development/iot && git pull && npx lerna run build
 sudo /usr/local/bin/forever start /home/pi/Desktop/development/iot/packages/desk-buddy/dist/index.js
